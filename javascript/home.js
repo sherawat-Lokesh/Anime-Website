e33d6553123f3c55c6a9f3036b1b397f2680328b
SHERAWAT LOKESH <107927380+sherawat-Lokesh@users.noreply.github.com>
2023-02-07 13:01:01 -0800

    added home page js🔥

--- javascript/home.js
+++ javascript/home.js
@@ -1,1 +1,31 @@
 'use strict'
+const cards= document.querySelector('.cards')
+
+async function run(){
+
+  const response = await  fetch("https://gogoanime.consumet.stream/top-airing")
+  const res= await response.json()
+  console.log(res)
+
+    res.forEach((element,i) => {
+
+        const html=`
+                    <div class="card">
+                            <img src="${element.animeImg}" alt="${i}">
+                        <div class="card-info">
+                            <div class="titles">
+                                <h2>${element.animeTitle.length<25?element.animeTitle.toUpperCase() :(element.animeTitle).substring(0,20).toUpperCase()+'...'}</h2>
+                                <p>Type : ${element.genres[0]}, ${element.genres[1]}</p>
+                                <p>Recent  ${element.latestEp}</p>
+                            </div>
+                            <div>
+                                <a class="watch-here" href="${element.animeUrl}" >Watch Here</a>
+                            </div>
+                        </div>
+                    </div>
+                    `
+                        cards.insertAdjacentHTML('beforeend',html)
+                    });
+
+}
+run()
