ac47f967996aa3e8baff7afcef7a665b6518fccf
SHERAWAT LOKESH <107927380+sherawat-Lokesh@users.noreply.github.com>
2023-02-08 08:22:41 -0800

    devTool acces removing  ⚔️

---
+++ javascript/RestrictAction.js
@@ -0,0 +1,7 @@
+"use strict"
+
+document.onkeydown=function(e){
+    if (e.keyCode == 123)return false;
+    if (e.metaKey && e.altKey && e.keyCode == 'I'. charCodeAt (0))return false;
+    if(e.shiftKey && e.altKey && e.keyCode =='i'.charCodeAt(0))return false
+}
